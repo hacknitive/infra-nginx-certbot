6941ed9977854e1b16d725f16cb91e0dd09ac01c
Reza Aghamohammadi <hacknitive@gmail.com>
2025-07-22 21:06:58 +0330

    20250722

--- certbot/renew_test.sh
+++ certbot/renew_test.sh
@@ -1,5 +1,5 @@
 while true
-do echo "Starting certificate renewal process..."
+do echo "Starting certificate renewal process (for test purpose)..."
 certbot renew --force-renewal --post-hook "docker exec ${NGINX_CONTAINER_NAME} nginx -s reload"
 sleep 5d
 done
